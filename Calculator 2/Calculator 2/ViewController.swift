e234b2c02a187db874b2b15ccf0232ef61037388
Asem S <aasem.javed@gmail.com>
2022-09-26 01:31:55 -0400

    Add files via upload

---
+++ Calculator 2/Calculator 2/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//
+//  Calculator 2 - A simple calculator app
+//  V 1.0
+//  Design created by Group 25
+//  on 25th September 2022
+//
+//  Group Members
+//  Asem Javed Syed - 301274803
+//  Dhanush Sriram - 301299251
+//  Murtuza Haider Naqvi- 301289355
+//
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    // function for operators
+    @IBAction func operatorBtn(_ sender: UIButton) {
+    }
+    
+    // function for clear and all clear
+    @IBAction func btnClear(_ sender: UIButton) {
+    }
+    
+    //function for backspace button
+    @IBAction func btnDel(_ sender: UIButton) {
+    }
+    
+    //function for numbers buttons
+    @IBAction func numberBtn(_ sender: UIButton) {
+    }
+    
+    //function to display result
+    @IBAction func resultLabel(_ sender: UITextField) {
+    }
+}
+
