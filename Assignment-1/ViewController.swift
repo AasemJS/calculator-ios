ff148455f5cf2fb1d1a6beca1cb4c0c84559bd5f
Dhanush <47747718+Dhanush009@users.noreply.github.com>
2022-10-09 14:52:15 -0400

    Updated assg2

--- Assignment-1/ViewController.swift
+++ Assignment-1/ViewController.swift
@@ -45,6 +45,25 @@ class ViewController: UIViewController {
     
     // function for operators
     @IBAction func operatorBtn(_ sender: UIButton) {
+        let operation = sender as UIButton
+        let operationText = operation.titleLabel!.text
+        
+        switch (operationText) {
+        case ("="):
+            if(!resultLabel.text!.contains("=")){
+                if let str = resultLabel.text{
+                    resultLabel.text = doCalculation(x: str)
+                }
+            }
+        default:
+            if(resultLabel.text == "0"){
+                resultLabel.text = "0"
+            }
+            else{
+                resultLabel.text?.append(operationText!)
+            }
+        }
+        
     }
     
     // function for clear and delete
@@ -69,4 +88,89 @@ class ViewController: UIViewController {
             }
         }
     }
+    
+    // functions for calculationss
+    func doCalculation(x: String) -> String{
+        let operands:[Character] = ["+","-","X","/","%"]
+        var lhs:String = ""
+        var operand = [String]()
+        var nums = [String]()
+
+        for char in x{
+            if operands.contains(char){
+                operand.append(String(char))
+                nums.append(lhs)
+                lhs=""
+            }
+                
+            else{
+                lhs+=String(char)
+            }
+        }
+
+        nums.append(lhs)
+        
+        return calcResult(numbers:nums,operands:operand)
+    }
+    
+    func calcResult(numbers: [String], operands: [String]) -> String{
+        var nums:[String] = numbers
+        var opers:[String] = operands
+        
+        while(opers.count > 0){
+        
+            nums[0] = calculate(num1:nums[0],num2:nums[1],operand:opers[0])
+            nums.remove(at: 1)
+            opers.remove(at: 0)
+        }
+        
+        return nums[0]
+        
+    }
+    
+    func calculate(num1:String,num2:String,operand:String) -> String{
+        
+        var res:Double = 0
+        let n1 = Double(num1)
+        let n2 = Double(num2)
+        
+        switch(operand) {
+                case("+"):
+                if let v1 = n1{
+                    if let v2 = n2{
+                        res = v1+v2
+                    }
+                }
+                case("-"):
+                if let v1 = n1{
+                    if let v2 = n2{
+                        res = v1-v2
+                    }
+                }
+                case("X"):
+                if let v1 = n1{
+                    if let v2 = n2{
+                        res = v1*v2
+                    }
+                }
+                case("/"):
+                if let v1 = n1{
+                    if let v2 = n2{
+                        res = v1/v2
+                    }
+                }
+                case("%"):
+                if let v1 = n1{
+                    
+                    res = v1/100
+                    
+                }
+                default : res = 0
+            }
+        return String(res);
+    }
+
+    
 }
+
+
